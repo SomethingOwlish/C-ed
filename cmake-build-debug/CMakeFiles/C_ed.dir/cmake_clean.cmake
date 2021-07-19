909c6d65fd6f94de7c5d321ffd989e4a8428004a
SomethingOwlish <55540709+SomethingOwlish@users.noreply.github.com>
2021-07-19 20:45:27 +0300

    1

---
+++ cmake-build-debug/CMakeFiles/C_ed.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_ed.dir/main.cpp.o"
+  "C_ed"
+  "C_ed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_ed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
