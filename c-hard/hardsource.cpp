f3b0945d3039981077099828b268d4047eaabc5c
a.dobroserdova <a.dobroserdova@eapteka.ru>
2022-03-31 22:38:53 +0200

    hardEx2

--- c-hard/hardsource.cpp
+++ c-hard/hardsource.cpp
@@ -1,4 +1,26 @@
 
-#include <iostream>
-#include <cstdint>
-#include <string>
+#include "hardsource.h"
+
+template <typename t>
+void showVector( std::vector<t>&a)
+{
+    for (typename std::vector<t>::iterator it = a.begin() ; it!=a.end() ; ++it)
+        std::cout << *it;
+};
+
+void showVectorLink( std::vector<int*>&a)
+{
+    for (typename std::vector<int*>::iterator it = a.begin() ; it!=a.end() ; ++it)
+    { std::cout << **it << " ";};
+    std::cout << std::endl;
+};
+
+
+    void timer::startTimer(){
+        start = clock();
+    };
+
+    void timer::stopTimer(){
+        stop = clock();
+        std::cout << "Time: " << stop - start << std::endl;
+    };
